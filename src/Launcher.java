98e539dae74dd18c6a818091bc3055dcd03d8727
francois.machuron <francois.machuron@epita.fr>
2021-05-17 16:20:00 +0200

    adding launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class Launcher {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("ceci est un message de bienvenue, maintenant tape du texte :");
         Scanner variable = new Scanner(System.in);
         String text = variable.nextLine();
-        System.out.println("ton texte :" + text);
+        while (!text.equals("quit")) {
+            System.out.println("unknow command");
+            System.out.println("faut que tu mettes quit");
+        }
+
     }
 }
